6a53b390b47292f9b9a022cc37de3ac4133bbd57
dinnoc <james.dinniss@gmail.com>
2022-08-19 21:19:47 +0100

    Major upgrade for speed

--- ESPNowSend.ino
+++ ESPNowSend.ino
@@ -4,18 +4,21 @@
 #include <OneWire.h>
 #include <DallasTemperature.h>
 
+// speeed management upgrade
 
-//set Reciever MacAddress  94:B9:7E:D9:3B:98
+//------------------------------------ESPNOW SET UP STUFF-----------------------------------
+// set Reciever MacAddress  94:B9:7E:D9:3B:98
 
 uint8_t broadcastAddress[] = {0x94, 0xB9, 0x7E, 0xD9, 0x3B, 0x98};
 
 // Structure example to send data
 // Must match the receiver structure
-typedef struct struct_message {
-  char a[32]; //board name/device
-  int b;  //int value
-  float c;  //float value
-  bool d;  //bool value
+typedef struct struct_message
+{
+  char a[32]; // board name/device
+  int b;      // int value
+  float c;    // float value
+  bool d;     // bool value
   float e;
   float f;
   float g;
@@ -23,15 +26,29 @@ typedef struct struct_message {
 
 // Create a struct_message called myData
 struct_message myData;
+esp_now_peer_info_t peerInfo;
+int retry = 0;
 
-//Manage Wifi channels
+//--------------------------------WIFI MANAGEMENT------------------------------
 
 constexpr char WIFI_SSID[] = "eir76484769";
 
-int32_t getWiFiChannel(const char *ssid) {
-  if (int32_t n = WiFi.scanNetworks()) {
-    for (uint8_t i = 0; i < n; i++) {
-      if (!strcmp(ssid, WiFi.SSID(i).c_str())) {
+int32_t setWiFiChannel(const char *ssid)
+{
+  Serial.print("Get WifiChan hit");
+  if (int32_t n = WiFi.scanNetworks())
+  {
+    for (uint8_t i = 0; i < n; i++)
+    {
+      Serial.println(WiFi.SSID(i));
+      if (!strcmp(ssid, WiFi.SSID(i).c_str()))
+      {
+        Serial.println("-----------getWifiChan ---------------");
+        WiFi.printDiag(Serial);
+        esp_wifi_set_promiscuous(true);
+        esp_wifi_set_channel(WiFi.channel(i), WIFI_SECOND_CHAN_NONE);
+        esp_wifi_set_promiscuous(false);
+        WiFi.printDiag(Serial);
         return WiFi.channel(i);
       }
     }
@@ -39,81 +56,50 @@ int32_t getWiFiChannel(const char *ssid) {
   return 0;
 }
 
-esp_now_peer_info_t peerInfo;
-
-//set up DSB1820 sensor
+//--------------------------------SENSOR SET UP------------------------------
+// set up DSB1820 sensor
 OneWire oneWire(4);
 DallasTemperature sensors(&oneWire);
 
-//Deep sleep parameters
+//----------------------------DEEP SLEEP SET UP-----------------------------
+// Deep sleep parameters
 
-#define uS_TO_S_FACTOR 1000000ULL  /* Conversion factor for micro seconds to seconds */
-#define TIME_TO_SLEEP  1200
-RTC_DATA_ATTR int bootCount = 0;
+#define uS_TO_S_FACTOR 1000000ULL /* Conversion factor for micro seconds to seconds */
+#define TIME_TO_SLEEP 5 //1200
+RTC_DATA_ATTR int resetChan = 0;
+int mStart, mEnd, mDiff = 0;
 
+//-------------------------------ON DATA SENT ---------------------------------
 
-void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
+void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status)
+{
 
   Serial.println("OnDataSent Fired");
   Serial.print("\r\nLast Packet Send Status:\t");
   Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
 
-
-}
-
-
-void setup() {
-  Serial.begin(115200);
-  delay(1000);
-  ++bootCount;
-  Serial.println("Boot number: " + String(bootCount));
-
-  esp_sleep_enable_timer_wakeup(TIME_TO_SLEEP * uS_TO_S_FACTOR);
-
-  Serial.println("Sender node");
-  WiFi.mode(WIFI_STA);
-
-  //get channel of current network
-  int32_t ssidChan = getWiFiChannel(WIFI_SSID);
-  Serial.print("SSID Wifi chan: ");
-  Serial.println(ssidChan);
-
-  //set channel of this unit to same as current network channel
-  // esp_wifi_set_channel(4, WIFI_SECOND_CHAN_NONE);
-  WiFi.printDiag(Serial);
-  esp_wifi_set_promiscuous(true);
-  esp_wifi_set_channel(ssidChan, WIFI_SECOND_CHAN_NONE);
-  esp_wifi_set_promiscuous(false);
-  WiFi.printDiag(Serial);
-
-  //initiate ESP0-now
-  if (esp_now_init() != ESP_OK) {
-    Serial.println("Error initialisting ESP-NOW");
-    return;
+  if (status != ESP_NOW_SEND_SUCCESS)
+  {
+    // set flag to reset wifi channel on next load set channel of current network
+    resetChan = 1;
+    esp_sleep_enable_timer_wakeup(5 * uS_TO_S_FACTOR);
+    Serial.println("Going to sleep now for 5 seconds then will try again");
+    Serial.flush();
+    esp_deep_sleep_start();
   }
-
-
-  //set up call back for ESPnow after data send
-  esp_now_register_send_cb(OnDataSent);
-
-  //set up peer to link to reciever
-  memcpy(peerInfo.peer_addr, broadcastAddress, 6);
-
-  //add peer
-  esp_now_add_peer(&peerInfo);
-
-  //send start up message via mqtt
-
-  strcpy(myData.a, "Board_1_Startup");
-  myData.b = 1;
-  esp_err_t result = esp_now_send(broadcastAddress, (uint8_t *) &myData, sizeof(myData));
-
-
-  //start Sensors
-  sensors.begin();
+  else
+  {
+    mEnd = millis();
+    mDiff = mEnd - mStart;
+    Serial.print("Going to sleep now. Last Cycle took (ms)");
+    Serial.println(mDiff);
+    Serial.flush();
+    esp_deep_sleep_start();
+  }
 }
 
-void loop() {
+void readAndSend()
+{
 
   sensors.requestTemperatures();
   Serial.print("Temp reading:");
@@ -122,16 +108,48 @@ void loop() {
   strcpy(myData.a, "Board_1");
   myData.c = sensors.getTempCByIndex(0);
 
-  esp_err_t result = esp_now_send(broadcastAddress, (uint8_t *) &myData, sizeof(myData));
+  esp_err_t result = esp_now_send(broadcastAddress, (uint8_t *)&myData, sizeof(myData));
+}
 
-  if (result == ESP_OK) {
-    Serial.println("Sent with success");
-  }
-  else {
-    Serial.println("Error sending the data");
+void setup()
+{
+  mStart = millis();
+  Serial.begin(115200);
+
+  esp_sleep_enable_timer_wakeup(TIME_TO_SLEEP * uS_TO_S_FACTOR);
+
+  Serial.println("Sender node");
+  WiFi.mode(WIFI_STA);
+
+
+  if (resetChan != 0)
+  {
+    int32_t ssidChan = setWiFiChannel(WIFI_SSID);
+    resetChan = 0;
   }
 
-  Serial.println("Going to sleep now");
-  Serial.flush();
-  esp_deep_sleep_start();
+  // initiate ESP0-now
+  if (esp_now_init() != ESP_OK)
+  {
+    Serial.println("Error initialisting ESP-NOW");
+    return;
+  }
+
+  // set up call back for ESPnow after data send
+  esp_now_register_send_cb(OnDataSent);
+
+  // set up peer to link to reciever
+  memcpy(peerInfo.peer_addr, broadcastAddress, 6);
+
+  // add peer
+  esp_now_add_peer(&peerInfo);
+
+  // start Sensors
+  sensors.begin();
+}
+
+void loop()
+{
+  readAndSend();
+  delay(5000);
 }
